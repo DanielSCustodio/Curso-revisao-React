3925d47f1c3856e4d5a6983544d766bc7b31361a
Daniel Custodio <play.daniel@hotmail.com>
2022-09-21 15:08:21 -0300

    feat: useState

--- app/src/App.js
+++ app/src/App.js
@@ -2,11 +2,16 @@ import React from 'react'
 import Name from './components/Name'
 
 export default function App() {
+  const [help, setHelp] = React.useState('Socorro');
+
   return (
     <div>
-      <h1>Nomes:</h1>
-      <Name name="Daniel"/>
+      <h1>DC Comics</h1>
+      <span>{help}</span>
+      <br/>
       <Name name="Batman"/>
+
+      <button onClick={()=> setHelp(help+"o")}>GRITAR</button>
     </div>
   )
 }
